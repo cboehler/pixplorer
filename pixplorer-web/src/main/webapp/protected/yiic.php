4c379f9de60d97ba6127d4b09920a2f16d7f7a00
cbo <c.boehler@vol.at>
2015-06-07 10:41:52 +0200

    added some more unit tests and the fancy yii stuff..

--- pixplorer-web/src/main/webapp/protected/yiic.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../yii-1.1.16.bca042/framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
-
-require_once($yiic);
